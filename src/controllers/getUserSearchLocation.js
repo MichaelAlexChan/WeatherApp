b5abd36dd19a66f0af7322c0f52e646c9aea21aa
Michael Chan <michaelalexanderchan@gmail.com>
2023-03-08 14:24:51 -0500

    Added grid formatting and styled the header. Also created an assets folder for icons

--- src/controllers/getUserSearchLocation.js
+++ src/controllers/getUserSearchLocation.js
@@ -1,6 +1,8 @@
 // Gets the user input value
 export default function getUserSearchLocation() {
+  const searchInput = document.querySelector('input');
   const searchValue = document.querySelector('input').value;
+  searchInput.value = '';
   console.log(`You have entered: ${searchValue}`);
   return searchValue;
 }
--- src/controllers/weatherContainer.js
+++ src/controllers/weatherContainer.js
@@ -3,7 +3,7 @@ import getWeather from '../model/getWeatherData';
 import '../style.css';
 
 const button = document.getElementById('weatherSubmit');
-const textContainer = document.getElementById('userWeather');
+const textContainer = document.getElementById('currentTemp');
 
 /* getWeather is an async function, so we have to wait for it to
 complete before changing the text of our element */
